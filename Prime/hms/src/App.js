cc56e84c9e45d3325599f712f1aac4dad3e9aa8f
Samarth Patel <samarthpatel.010@gmail.com>
2020-01-09 00:33:10 -0500

    SImple Redirect to Login Page

--- Prime/hms/src/App.js
+++ Prime/hms/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+//import {BrowserRouter, Switch, Route} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from "./Components/Auth/Login";
+import Dashboard from "./Components/Auth/Dashboard";
+
+// export default class App extends Component{
+//   render(){
+//     return(
+//       <div className ="app">
+//         <BrowserRouter>
+//         <Switch>
+//           <Route exact path={"/"} Component={Login}/>
+//           <Route exact path ={"/dashboard"} Component={Dashboard}/>
+//         </Switch>
+//         </BrowserRouter>
+//       </div>
+//     );
+//   }
+// }
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Login />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ Prime/hms/src/Components/Auth/Login.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Login extends Component{
+  render(){
+      return(
+      <h1 className="App-intro">
+        Login Page
+      </h1>
+      )
+  }
+} 
+
+export default Login;
