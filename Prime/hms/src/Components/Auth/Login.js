c151e0465823baf8b7b554577bf4d12ff7f6ee40
Samarth Patel <samarthpatel.010@gmail.com>
2020-01-11 15:22:36 -0500

    Login Page

--- Prime/hms/src/Components/Auth/Login.js
+++ Prime/hms/src/Components/Auth/Login.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {FormControl, Button, FormGroup} from 'react-bootstrap';
+import "./Auth.css"
 
 
 class Login extends Component{
   render(){
       return(
-      <h1 className="App-intro">
-        Login Page
-      </h1>
+        <div className="Login">
+        <form>
+          <FormGroup>
+            <h1> Login </h1>
+          </FormGroup>
+        <FormGroup bsSize="large">
+            <FormControl
+              autoFocus
+              type="Email"
+              placeholder ="Email"
+              //text ="Email"
+              //value={email}
+              //onChange={e=> setEmail(e.target.value)}
+              />
+        </FormGroup>
+
+        <FormGroup bsSize="large">
+            <FormControl
+              type="password"
+              placeholder ="Password"
+             // value={password}
+              //onChange={e=> setPassword(e.target.value)}
+              />
+        </FormGroup>
+
+        <Button block bsSize="large" type="submit">
+          Login
+        </Button>
+
+        </form>
+      </div>
       )
   }
 } 
