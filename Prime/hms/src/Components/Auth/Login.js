c9b3e34563ffe9a4a5e0b9e9b53e0c49ab309899
Samarth Patel <samarthpatel.010@gmail.com>
2020-01-11 22:12:50 -0500

    CSS UPDATE

--- Prime/hms/src/Components/Auth/Login.js
+++ Prime/hms/src/Components/Auth/Login.js
@@ -16,41 +16,60 @@ class Login extends Component{
   render(){
       return(
        
-        <div className="Login">       
-        <form>
-          <FormGroup>
-            <h1> Login </h1>
-          </FormGroup>
-        <FormGroup bsSize="large">
-            <FormControl
-              autoFocus
-              type="Email"
-              placeholder ="Email"
-              //text ="Email"
-              //value={email}
-              //onChange={e=> setEmail(e.target.value)}
-              />
-        </FormGroup>
+        // <h1> House Management System </h1>
+        // <h3 class="text-center text-white pt-5"> Login </h3>
+        <div>
+          <div class="container">
+              <div id="login-row" class="row justify-content-center align-items-center">
+                  <div id="login-column" class="col-md-6">
+                      <div id="login-box" class="col-md-12">
+                        <form class="form">
+                          <FormGroup>
+                            <h1> House Management System </h1>
+                            <h3 class="text-center text-white pt-5"> Login </h3>
+                          </FormGroup>
+                          
+                          <h3 class="text-center text-info">Login</h3>
 
-        <FormGroup bsSize="large">
-            <FormControl
-              type="password"
-              placeholder ="Password"
-             // value={password}
-              //onChange={e=> setPassword(e.target.value)}
-              />
-        </FormGroup>
-        <Button  type="submit" name="Login">
-          Login
-        </Button>        
-        <Link to="/SignUp">
-          <h6>New User ? SignUp </h6>         
-        </Link>
-        <Link to="/ForgotPassword">          
-          <h6>Forgot you passowrd?</h6>         
-        </Link>       
-        </form>
-      </div>
+                          <div class="form-group">
+                            <FormGroup bsSize="large" class="row justify-content-center align-items-center text-info from-control">
+                                <FormControl
+                                  autoFocus
+                                  type="Email"
+                                  placeholder ="Email"
+                                  //text ="Email"
+                                  //value={email}
+                                  //onChange={e=> setEmail(e.target.value)}
+                                  />
+                            </FormGroup>
+                          </div>
+                        
+                          <div class="form-group">
+                            <FormGroup bsSize="large" class="row justify-content-center align-items-center text-info from-control">
+                                <FormControl
+                                  type="password"
+                                  placeholder ="Password"
+                                // value={password}
+                                  //onChange={e=> setPassword(e.target.value)}
+                                  />
+                            </FormGroup>
+                            </div>
+
+                        <Button class="btn btn-info btn-md"  type="submit" name="Login">
+                          Login
+                        </Button>        
+                        <Link to="/SignUp">
+                          <h6 class="text-info">New User ? SignUp </h6>         
+                        </Link>
+                        <Link to="/ForgotPassword">          
+                          <h6 class="text-info">Forgot you passowrd?</h6>         
+                        </Link>       
+                        </form>
+                      </div>
+                  </div>
+                </div>
+            </div>
+          </div>
       );
   }
 } 
