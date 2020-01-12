01f255d19d8407b8f237ee75a1e8cd8f70c70cdd
Samarth Patel <samarthpatel.010@gmail.com>
2020-01-11 22:58:07 -0500

    Login page css

--- Prime/hms/src/Components/Auth/Login.js
+++ Prime/hms/src/Components/Auth/Login.js
@@ -22,14 +22,14 @@ class Login extends Component{
           <div class="container">
               <div id="login-row" class="row justify-content-center align-items-center">
                   <div id="login-column" class="col-md-6">
-                      <div id="login-box" class="col-md-12">
+                      <div id="login-box" class="col-md-12 back">
                         <form class="form">
                           <FormGroup>
                             <h1> House Management System </h1>
-                            <h3 class="text-center text-white pt-5"> Login </h3>
+                            {/* <h3 class="text-center text-white pt-5"> Login </h3> */}
                           </FormGroup>
                           
-                          <h3 class="text-center text-info">Login</h3>
+                          <h3 class="text-center text-info text-color">Login</h3>
 
                           <div class="form-group">
                             <FormGroup bsSize="large" class="row justify-content-center align-items-center text-info from-control">
@@ -55,11 +55,14 @@ class Login extends Component{
                             </FormGroup>
                             </div>
 
-                        <Button class="btn btn-info btn-md"  type="submit" name="Login">
-                          Login
-                        </Button>        
+                          <div class= "form-group">
+                          <Button class="btn btn-info btn-md button"  type="submit" name="Login">
+                              Login
+                            </Button>
+                          </div>
+                                    
                         <Link to="/SignUp">
-                          <h6 class="text-info">New User ? SignUp </h6>         
+                          <h6 class="text-info link-text">New User ? SignUp </h6>         
                         </Link>
                         <Link to="/ForgotPassword">          
                           <h6 class="text-info">Forgot you passowrd?</h6>         
